612af4a80899a4a3d5c0b728a7d1056bdf115ab8
Milagros <miliarganin3@gmail.com>
2026-09-04 22:29:22 -0300

    Realizo contrato para los prestamos

--- app/src/main/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/RegistroPrestamos.java
+++ app/src/main/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/RegistroPrestamos.java
@@ -1,4 +1,11 @@
 package ar.uba.fi.cb100.clases.a2026.c02.tps.tp1;
-
+//Esta interface no lleva importa porque las clases que se necesitan ya están en el paquete, por lo tanto pieden verse entre si
 public interface RegistroPrestamos {
+
+        void registrar(Prestamo p); //registra un objeto prestamo en donde se use el metodo
+        int cantidad();
+        Prestamo obtener(int i);              // i es n de prestamo q se quire obtener, IndexOutOfBoundsException si i es invalido
+        int[] padrones();                     // devuelve un array con int de los padrones, sin repetidos, en orden de aparicion
+        Prestamo[] prestamosDe(int padron);   // devulve un array con la catidad de veces que hizo un prestamo el padron que se pasa comp parametro, arreglo vacio si no hay ninguno
+        String[] titulosMasPedidos(int n);    // arreglo con los n titulos mas pedidos, desempate alfabetico
 }
