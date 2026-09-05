6dba8b1877b93e885e036322643c49f4c6be4be1
Milagros <miliarganin3@gmail.com>
2026-09-04 22:30:20 -0300

    Creo la clase donde cumplo con la interface

--- app/src/main/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/RegistroSobreArreglo.java
+++ app/src/main/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/RegistroSobreArreglo.java
@@ -1,4 +1,33 @@
 package ar.uba.fi.cb100.clases.a2026.c02.tps.tp1;
 
-public class RegistroSobreArreglo {
+public class RegistroSobreArreglo implements RegistroPrestamos{
+    @Override
+    public void registrar(Prestamo p) {
+
+    }
+
+    @Override
+    public int cantidad() {
+        return 0;
+    }
+
+    @Override
+    public Prestamo obtener(int i) {
+        return null;
+    }
+
+    @Override
+    public int[] padrones() {
+        return new int[0];
+    }
+
+    @Override
+    public Prestamo[] prestamosDe(int padron) {
+        return new Prestamo[0];
+    }
+
+    @Override
+    public String[] titulosMasPedidos(int n) {
+        return new String[0];
+    }
 }
