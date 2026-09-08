19c6ebf7b2aceda55daed4a21d1baeb4f4123391
Milagros <miliarganin3@gmail.com>
2026-09-08 20:31:38 -0300

    Creo la interface ExportadorDeReporte

--- app/src/main/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/ExportadorDeReporte.java
+++ app/src/main/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/ExportadorDeReporte.java
@@ -1,4 +1,8 @@
 package ar.uba.fi.cb100.clases.a2026.c02.tps.tp1;
-
-public interface ExportadorReporte {
+import java.io.IOException;
+import java.nio.file.Path;
+public interface ExportadorDeReporte {
+    void exportar(FilaDeSocio[] filas, Path destino)
+            throws IOException;
+            String extension();   // "txt", "csv", ...
 }
