b079664f43ff005578da020494881daf1349a9e5
Milagros <miliarganin3@gmail.com>
2026-09-08 20:32:15 -0300

    Agrego test de ExportadorCsv

--- app/src/test/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/ExportadorCsvTest.java
+++ app/src/test/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/ExportadorCsvTest.java
@@ -1,4 +1,23 @@
-package clases.a2026.c02.tps.tp1;
+package ar.uba.fi.cb100.clases.a2026.c02.tps.tp1;
 
-public class ExportadorCsvTest {
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExportadorCsvTest {
+
+    @Test
+    void escribeYLeeCorrectamente() throws IOException {
+        FilaDeSocio[] filas = {
+                new FilaDeSocio(39876, "Bruno Ferrari", 4, 24, 3600, "CON_DEUDA")
+        };
+        Path temp = Files.createTempFile("reporte", ".csv");
+        new ExportadorCSV().exportar(filas, temp);
+
+        var lineas = Files.readAllLines(temp);
+        assertEquals("padron;socio;prestamos;dias_atraso;multa;estado", lineas.get(0));
+        assertEquals("39876;Bruno Ferrari;4;24;3600;CON_DEUDA", lineas.get(1));
+    }
 }
